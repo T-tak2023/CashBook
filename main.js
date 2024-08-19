5e1da1c5125546b348f72e0cce06ce0822121b53
T-tak2023 <takashi.t1985@icloud.com>
2024-08-19 16:12:44 +0900

    feat: sqlite3と連携。全データの表示機能を追加

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path')
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./cashbook.db', (err) => {
+  if (err) {
+    console.error('Could not connect to database', err);
+  } else {
+    console.log('Connected to the SQLite database');
+  }
+});
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false,
+      sandbox: false,
     }
   })
 
@@ -14,12 +27,25 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
+
+  ipcMain.handle('get-records', async () => {
+    return new Promise((resolve, reject) => {
+      db.all('SELECT * FROM transactions', [], (err, rows) => {
+        if (err) {
+          console.error('Database error:', err);
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  });
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
